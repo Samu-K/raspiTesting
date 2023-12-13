fd6d373d99d697c9fc61cbd12396a98549ce9201
Samu-K <samu.kaarlela@gmail.com>
2023-12-13 08:42:13 +0200

    Added in write and read funcs

--- main.c
+++ main.c
@@ -2,17 +2,40 @@
 
 typedef unsigned int uint;
 
+void delay(void) {
+  volatile int i=0;
+  while (i < 0x850000) {
+    i++;
+  }
+  return;
+}
+
+void write(void *dst, uint val) {
+  uint* udst = (uint*)dst;
+  *udst = val;
+  return;
+}
+
+uint read(void* src) {
+  uint* usrc = (uint*)src;
+  return *usrc;
+}
+
 int main(void) {
   int i=0;
-
-  while(1) {
-    /* turn on pin 20 */ 
+  uint sel2 = read(BCM2837_GPFSEL2);
+  sel2 |= (1<<3); /* set pin 21 to output */ 
+  write(BCM2837_GPFSEL2, sel2);
   
+  while(1) {
+    /* turn on pin 21 */ 
+    write(BCM2837_GPSET0, 1<<21);
     /* add delay */ 
-
-    /* turn off pin 20 */
-
+    delay();
+    /* turn off pin 21 */
+    write(BCM2837_GPCLR0,1<<21);
     /* add delay */ 
+    delay();
      
   }
 
