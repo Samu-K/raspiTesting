e453677bed1956457782bdf4aaf1d3c511bfdaa3
Samu-K <samu.kaarlela@gmail.com>
2023-12-13 08:22:48 +0200

    Boilerplate for led blinker

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "BCM2837.h"
+
+typedef unsigned int uint;
+
+int main(void) {
+  int i=0;
+
+  while(1) {
+    /* turn on pin 20 */ 
+  
+    /* add delay */ 
+
+    /* turn off pin 20 */
+
+    /* add delay */ 
+     
+  }
+
+  return 0;
+}
